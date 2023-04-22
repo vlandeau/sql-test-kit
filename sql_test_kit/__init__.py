b9a782a3d27951931f0fdb55fce7be0d29e591b1
victorlandeau <vlandeau@gmail.com>
2023-04-23 00:41:12 +0200

    feat(import): Allow importing all public objects from package root

---
+++ sql_test_kit/__init__.py
@@ -0,0 +1,8 @@
+from sql_test_kit.table import Table
+from sql_test_kit.schema import Schema
+from sql_test_kit.column import Column
+from sql_test_kit.query_interpolation import (
+    InterpolationData,
+    replace_table_names_in_string_by_data_literals,
+    get_data_literals_query,
+)
